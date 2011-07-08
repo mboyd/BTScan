1d41ea2b9dc963712174c317440cca7f311136c4
Clark <clarkds@mit.edu>
2011-07-08 17:11:16 -0400

    working system with fudged data - algorithm is handling gaussian error

---
+++ data_packet.py
@@ -0,0 +1,12 @@
+class DataPacket(object):
+    
+    def __init__(self, timestamp, receiver_mac, device_mac, rssi, position = None):
+        
+        self.timestamp = timestamp
+        self.receiver_mac = receiver_mac
+        self.device_mac = device_mac
+        self.rssi = rssi
+        self.position = position
+
+    def __repr__(self):
+        return "(DataPacket: t=%f, r=%s, d=%s, rssi=%d, pos=%s" % (self.timestamp[0], self.receiver_mac, self.device_mac, self.rssi, str(self.position))
