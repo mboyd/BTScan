2fcd79b55bc9252c3b2a738e8c572ae7f9477c0d
horklinator <bhork13@gmail.com>
2011-07-18 10:56:27 -0400

    7/15/11 version

--- tracking_pyqt.py
+++ tracking_pyqt.py
@@ -4,7 +4,7 @@
 
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
-import scan_server, config, data_packet, Mysql_logger
+import scan_server, config, data_packet#, Mysql_logger
 from PIL import Image
 from collections import deque
 import sys, time, Queue, random
@@ -46,15 +46,7 @@ class MainApp (QWidget):
         history.setStatusTip('History')
         self.connect(history, SIGNAL('triggered()'), self.History)
 
-
         
-        # Tabs
-        self.mainTab = QTabWidget()
-        self.sideTab = QTabWidget()        
-        tab1 = Map()
-        tab2 = QLabel()
-        tab3 = QTableView()
-        tab4 = QLabel()
 
         # Create actions
         # Add new blank tab
@@ -75,6 +67,14 @@ class MainApp (QWidget):
         rnTab.setStatusTip('Rename Tab')
         self.connect(rnTab, SIGNAL('triggered()'), self.rnCurTab)
         
+        # Tabs
+        self.mainTab = QTabWidget()
+        self.sideTab = QTabWidget()        
+        tab1 = Map()
+        tab2 = QLabel()
+        tab3 = QTableWidget()
+        tab4 = QLabel()
+        
         # Initialize menu bar, set menu options
         menubar = QMenuBar()
         file = menubar.addMenu('&File')
@@ -88,10 +88,6 @@ class MainApp (QWidget):
         tabs.addAction(rnTab)
         
         # Set tab layouts
-        p1_vertical = QVBoxLayout(tab1)
-        p2_vertical = QVBoxLayout(tab2)
-        p3_vertical = QVBoxLayout(tab3)
-        p4_vertical = QVBoxLayout(tab4)
 
         self.mainTab.addTab(tab1, "Main")
         self.mainTab.addTab(tab2, "Description")
@@ -103,36 +99,27 @@ class MainApp (QWidget):
         self.sideFrame = QVBoxLayout()
         self.sideFrame.addWidget(self.sideTab)
 
-        frame2 = QHBoxLayout()
-        frame2.addLayout(self.mainFrame, 2)
-        frame2.addLayout(self.sideFrame, 1)
-        frame1 = QVBoxLayout()
-        frame1.addWidget(menubar)
-        frame1.addLayout(frame2)
+        frame1 = QHBoxLayout()
+        frame1.addLayout(self.mainFrame, stretch=2)
+        frame1.addLayout(self.sideFrame, stretch=1)
+        frame2 = QVBoxLayout()
+        frame2.addWidget(menubar)
+        frame2.addLayout(frame1)
+
             
-        self.trackingArea = QWidget(self)
+        #self.trackingArea = QWidget(self)
         self.createSideMenu()
         
-        self.setLayout(frame1)
+        self.setLayout(frame2)
         
         # Creates box for raw data dump; show with showRSSI()
         self.RSSI = QWidget()
         self.RSSI.resize(300, 200)
         self.RSSI.setWindowTitle('RSSI')
-        
-        ### DEVICE INTEGRATION ###
-        self.device_list = dict()   # GUI elements for devices
-        
-        self.position_data = dict()
-        
-        self.Hlength = config.TRACKING_HISTORY  #length of visible tracking history
-        
-        self.evt_queue = Queue.Queue()
-        # self.root.after(config.POLL_PERIOD, self.check_queue) TKINTER- FIX
 
         self.rssi_plot = None # necessary?
         
-        self.add_device('0.0.0.0') #TEST
+        #self.add_device('0.0.0.0') #TEST
     
     def mapOpen(self): # Loads map in current tab
     
@@ -166,8 +153,9 @@ class MainApp (QWidget):
     
     def rmCurTab(self):
         self.mainTab.removeTab(self.mainTab.currentIndex())
-        
-    def rnCurTab(self):
+    
+    # Rename current tab    
+    def rnCurTab(self): 
     	input = QInputDialog(self)
     	input.setLabelText('New name?')
     	newName = QInputDialog.getText(self, 'Rename Tab', 'New name?')
@@ -182,8 +170,9 @@ class MainApp (QWidget):
 
 
     def createSideMenu(self):
+        print "creating side menu"
         tribe = self.sideTab.widget(0)
-        tribe.setHorizontalHeader(["track", "BD_ADDR", "#_RCVR", "COLOR"])
+        tribe.setHorizontalHeaderLabels(["track", "BD_ADDR", "#_RCVR", "COLOR"])
         tribe.setColumnWidth(0, 40)
         tribe.setColumnWidth(1, 100)
         tribe.setColumnWidth(2, 45)
@@ -204,7 +193,7 @@ class MainApp (QWidget):
          except Queue.Empty:
             pass
         
-            # self.root.after(config.POLL_PERIOD, self.check_queue) # FIXME
+         QTimer.singleShot(config.POLL_PERIOD, self.check_queue) # FIXME
      
     def handle_new_device(self, device_mac):
          print 'New device detected: %s' % device_mac
@@ -261,7 +250,7 @@ class Map(QLabel):
     # dList 
     def __init__ (self):
         super(Map, self).__init__()
-        pm = QPixmap('test-grid.gif').scaled(self.size())
+        pm = QPixmap('NE47_5.jpg').scaled(self.size())
         self.setPixmap(pm)
     #def __init__(self, pathname, dList):
     #    QLabel.__init__()
@@ -272,16 +261,16 @@ class Map(QLabel):
         painter = QPainter();        
         painter.begin(self)
         painter.save();
-        painter.drawPixmap(10, 10, QPixmap('test-grid.gif'));
+        painter.drawPixmap(10, 10, QPixmap('NE47_5.jpg'));
         painter.setFont(QFont('Decorative', 10))
         self.drawPoint(painter)
         painter.drawText(50, 50, 'test');
         painter.restore();
         painter.end()
-    def drawPoint(self, qp):
+    def drawPoint(self, qp): # test method
         qp.setPen(Qt.red)
         qp.setBrush(QColor(255, 0, 0, 80))
-            qp.drawRect(10, 15, 90, 60)
+        qp.drawRect(10, 15, 90, 60)
      ## need to get x,y values
       
         #pass
